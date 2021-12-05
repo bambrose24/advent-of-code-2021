33d7f2580ab23e385ad5d820cf557c0b5fe0d404
Bob Ambrose <bambrose24@gmail.com>
2021-12-05 10:53:25 -0500

    solution files for each dir

---
+++ day7/solution.py
@@ -0,0 +1,15 @@
+
+def main():
+    p1_answer = part_one()
+    print('Part 1 Answer:', p1_answer)
+    p2_answer = part_two()
+    print('Part 2 Answer:', p1_answer)
+
+def part_one():
+    return None
+
+def part_two():
+    return None
+
+if __name__ == '__main__':
+    main()
