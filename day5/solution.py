e63fbe32bc0d5f3b71660e1c310b30f54bb96d8b
Bob Ambrose <bambrose24@gmail.com>
2021-12-08 10:55:28 -0500

    day5 and day6

--- day5/solution.py
+++ day5/solution.py
@@ -1,15 +1,90 @@
 
+from typing import List, Tuple
+
+
 def main():
     p1_answer = part_one()
     print('Part 1 Answer:', p1_answer)
+    
     p2_answer = part_two()
     print('Part 2 Answer:', p2_answer)
 
+def get_lines() -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+    with open('input.txt', 'r') as f:
+        res = []
+        for l in f:
+            a,b = l.strip().split('->')
+            x1,y1 = a.strip().split(',')
+            x2,y2 = b.strip().split(',')
+            x1,y1,x2,y2 = int(x1), int(y1), int(x2), int(y2)
+            res.append(((x1,y1), (x2,y2)))
+        return res
+            
+
+
 def part_one():
-    return None
+    ranges = get_lines()
+    max_x = max([r[0][0] for r in ranges] + [r[1][0] for r in ranges])
+    max_y = max([r[0][1] for r in ranges] + [r[1][1] for r in ranges])
+
+    table = [[0 for y in range(max_y + 1)] for x in range(max_x + 1)]
+
+    for (x1,y1), (x2,y2) in ranges:
+        if x1 != x2 and y1 != y2:
+            continue
+
+        if x1 == x2:
+            y1,y2 = min(y1,y2), max(y1,y2)
+            x = x1
+            for y in range(y1, y2 + 1):
+                table[x][y] += 1
+
+        else: # y1 == y2
+            y = y1
+            x1,x2 = min(x1,x2), max(x1,x2)
+            for x in range(x1, x2 + 1):
+                table[x][y] += 1
+        
+    total = 0
+    for x in range(len(table)):
+        for y in range(len(table[x])):
+            if table[x][y] > 1:
+                total += 1
+    return total
 
 def part_two():
-    return None
+    ranges = get_lines()
+    max_x = max([r[0][0] for r in ranges] + [r[1][0] for r in ranges])
+    max_y = max([r[0][1] for r in ranges] + [r[1][1] for r in ranges])
+
+    table = [[0 for y in range(max_y + 1)] for x in range(max_x + 1)]
+
+    for (x1,y1), (x2,y2) in ranges:
+        if x1 == x2:
+            y1,y2 = min(y1,y2), max(y1,y2)
+            x = x1
+            for y in range(y1, y2 + 1):
+                table[x][y] += 1
+        elif y1 == y2: # y1 == y2
+            x1,x2 = min(x1,x2), max(x1,x2)
+            y = y1
+            for x in range(x1, x2 + 1):
+                table[x][y] += 1
+        else:
+            x_inc = 1 if x1 < x2 else -1
+            y_inc = 1 if y1 < y2 else -1
+            x,y = x1,y1
+            for i in range(abs(x1 - x2) + 1):
+                table[x][y] += 1
+                x += x_inc
+                y += y_inc
+        
+    total = 0
+    for x in range(len(table)):
+        for y in range(len(table[x])):
+            if table[x][y] > 1:
+                total += 1
+    return total
 
 if __name__ == '__main__':
     main()
--- day6/solution.py
+++ day6/solution.py
@@ -1,15 +1,62 @@
 
+from collections import defaultdict
+
 def main():
     p1_answer = part_one()
     print('Part 1 Answer:', p1_answer)
+    
     p2_answer = part_two()
     print('Part 2 Answer:', p2_answer)
 
+def get_input():
+    with open('input.txt', 'r') as f:
+        return [int(x) for x in f.readline().strip().split(',')]
+
 def part_one():
-    return None
+    l = get_input()
+    d = defaultdict(int)
+    for v in l:
+        d[v] += 1
+    
+    num_days = 80
+    for day in range(num_days):
+        new_d = {}
+        for i in range(9):
+            if i not in d:
+                continue
+            if i == 0:
+                new_d[6] = d[i]
+                new_d[8] = d[i]
+            else:
+                if (i-1) not in new_d:
+                    new_d[i-1] = 0
+                new_d[i-1] += d[i]
+        d = new_d
+    return sum(d.values())
+
+
 
 def part_two():
-    return None
+    l = get_input()
+    d = defaultdict(int)
+    for v in l:
+        d[v] += 1
+    
+    num_days = 256
+    for day in range(num_days):
+        new_d = {}
+        for i in range(9):
+            if i not in d:
+                continue
+            if i == 0:
+                new_d[6] = d[i]
+                new_d[8] = d[i]
+            else:
+                if (i-1) not in new_d:
+                    new_d[i-1] = 0
+                new_d[i-1] += d[i]
+        d = new_d
+    return sum(d.values())
 
 if __name__ == '__main__':
     main()
