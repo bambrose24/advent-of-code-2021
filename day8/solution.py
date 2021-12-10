ce0fd173fee27fd29b2d75f5ea18295784a2d1a8
Bob Ambrose <bambrose24@gmail.com>
2021-12-10 13:29:08 -0500

    day8 and day9

--- day8/solution.py
+++ day8/solution.py
@@ -1,15 +1,119 @@
 
+from typing import List, Tuple
+
+
+segment_set_to_num = {
+    'abcefg': 0,
+    'cf': 1,
+    'acdeg': 2,
+    'acdfg': 3,
+    'bcdf': 4,
+    'abdfg': 5,
+    'abdefg': 6,
+    'acf': 7,
+    'abcdefg': 8,
+    'abcdfg': 9,
+}
+
 def main():
     p1_answer = part_one()
     print('Part 1 Answer:', p1_answer)
     p2_answer = part_two()
     print('Part 2 Answer:', p2_answer)
 
+def get_input() -> Tuple[List[str], List[str]]:
+    res = []
+    with open('input.txt', 'r') as f:
+        for line in f.readlines():
+            l,r = line.split('|')
+            res.append((l.strip().split(' '), r.strip().split(' ')))
+        return res
+
 def part_one():
-    return None
+    nums_input = get_input()
+    total = 0
+    for training,output in nums_input:
+        for num in output:
+            if len(num) in {2,3,4,7}:
+                total += 1
+    return total
 
 def part_two():
-    return None
+    nums_input = get_input()
+    total = 0
+    for training,output in nums_input:
+        figured_out_map = {} # Dict[Tuple[str], int]
+        segment_map = {} # Dict[str, str]
+
+        # figure out the 7 top segment
+        seven = [x for x in training if len(x) == 3][0]
+        one = [x for x in training if len(x) == 2][0]
+        top_char = list(set(seven) - set(one))[0]
+        segment_map[top_char] = 'a'
+
+        # find the top part of 1 -- 6, 9, and 0 have six segments
+        # but only 6 will have a single overlap with 1
+        six = [x for x in training if len(x) == 6 and len(set(x).intersection(set(one))) == 1][0]
+        bottom_part_of_one = list(set(six).intersection(set(one)))[0]
+        top_part_of_one = list(set(one) - set(bottom_part_of_one))[0]
+        segment_map[top_part_of_one] = 'c'
+        segment_map[bottom_part_of_one] = 'f'
+        
+        # next figure out middle rung based on seven overlap with the size five ones
+        # (3, 5, 2) -- only 3 will have full intersection (len 3). From there, we can
+        # intersect with 4, and get the middle rung, and upper left one from the remaining of 4
+        three = [x for x in training if len(x) == 5 and len(set(x).intersection(set(seven))) == 3][0]
+        four = [x for x in training if len(x) == 4][0]
+        middle_bar = list(set(three).intersection(set(four)) - set(one))[0]
+        segment_map[middle_bar] = 'd'
+        top_left = list(set(four) - set(one + middle_bar))[0]
+        segment_map[top_left] = 'b'
+
+        # now use 9 to get the bottom bar because we have all of 9 except that
+        figured_out = set(segment_map.keys())
+        nine = [x for x in training if len(x) == 6 and set(x).intersection(figured_out) == figured_out][0]
+        bottom_bar = list(set(nine) - figured_out)[0]
+        segment_map[bottom_bar] = 'g'
+
+        # remaining one is bottom left
+        bottom_left = [x for x in 'abcdefg' if x not in segment_map][0]
+        segment_map[bottom_left] = 'e'
+
+        # great! now we can map the output to sets of strings, sort the strings, and
+        # index into segment_set_to_num to find the value, and accrue a result string to find an integer
+        # print('segment map')
+        # for k in segment_map:
+        #     print(' ', k, segment_map[k])
+        # print()
+
+        # test = {
+        #     'one': one,
+        #     'seven': seven,
+        #     'three': three,
+        #     'four': four,
+        #     'six': six,
+        #     'nine': nine,
+        # }
+        # for k,v in test.items():
+        #     digit_res = ''
+        #     for c in v:
+        #         digit_res += str(segment_map[c])
+        #     digit_res = ''.join(sorted(digit_res))
+        #     print('digit res for', k, f"({v})", digit_res)
+        #     print(segment_set_to_num[digit_res])
+
+
+        digits = ''
+        for mixed_digit_str in output:
+            digit_res = ''
+            for c in mixed_digit_str:
+                digit_res += str(segment_map[c])
+            digit_res = ''.join(sorted(digit_res))
+            digits += str(segment_set_to_num[digit_res])
+
+        total += int(digits)
+    return total
+    
 
 if __name__ == '__main__':
     main()
--- day9/solution.py
+++ day9/solution.py
@@ -5,11 +5,57 @@ def main():
     p2_answer = part_two()
     print('Part 2 Answer:', p2_answer)
 
+def get_grid():
+    with open('input.txt', 'r') as f:
+        return [[int(y) for y in x.strip()] for x in f.readlines()]
+
+
 def part_one():
-    return None
+    grid = get_grid()
+    low_vals = get_low_vals(grid)
+    print('part one num low vals', len(low_vals))
+    return sum([grid[x][y] + 1 for (x,y) in low_vals])
+
+def get_low_vals(grid):
+    low_vals = []
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            v = grid[r][c]
+            neighbor_vals = [grid[x][y] for (x,y) in get_neighbors(grid, r, c)]
+            if min(neighbor_vals) > v:
+                low_vals.append((r,c))
+    return low_vals
+    
+
+def get_neighbors(grid, r, c):
+    rmax = len(grid) - 1
+    cmax = len(grid[0]) - 1
+    l = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+    return [x for x in l if x[0] >= 0 and x[1] >= 0 and x[0] <= rmax and x[1] <= cmax]
+
+def get_neighbors_vals(grid, r, c):
+    rmax = len(grid) - 1
+    cmax = len(grid[0]) - 1
+    l = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+    return [(x[0], x[1], grid[x[0]][x[1]]) for x in l if x[0] >= 0 and x[1] >= 0 and x[0] <= rmax and x[1] <= cmax]
 
 def part_two():
-    return None
+    grid = get_grid()
+    low_vals = get_low_vals(grid)
+    counts = []
+    for (r,c) in low_vals:
+        count = get_basin_count(grid, (r,c), 0)
+        counts.append(count)
+    top_three = list(sorted(counts))[-3:]
+    return top_three[0] * top_three[1] * top_three[2]
+
+def get_basin_count(grid, xy, curr_count):
+    x,y = xy
+    if grid[x][y] == -1 or grid[x][y] == 9:
+        return curr_count
+    grid[x][y] = -1
+    return 1 + sum([get_basin_count(grid, (x,y), curr_count) for (x,y,v) in get_neighbors_vals(grid, x, y)])
+
 
 if __name__ == '__main__':
     main()
